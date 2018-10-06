3e2b19ff18bfc388dd5b782ae54c53e12ee58f29
Utsav Shukla <us241077@gmail.com>
2018-10-06 23:41:43 +0530

    hashmap.py
    Very simple implementation of hashmaps(with chaining)

---
+++ hashmap.py
@@ -0,0 +1,13 @@
+table = [[] for x in range(100)]
+
+
+def hash_function(x): 
+    return x % 10 
+
+def insert(table,input,value): 
+    table[hash_function(input)].append((input,value))
+
+
+insert(table,41,'A')
+insert(table,93,'B')
+insert(table,13,'tangerine')
